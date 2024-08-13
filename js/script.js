d4a589baf98775181e0dee274af0c94141a3655f
lucas-r-f <lucasrodriguesdefreitas2002@hotmail.com>
2024-08-13 17:14:32 -0300

    using api

--- js/script.js
+++ js/script.js
@@ -1,1 +1,39 @@
-//78a58b51a528ac4982f8996d3f817b23
+//Variáveis e seleção de elementos
+const apiKey = "78a58b51a528ac4982f8996d3f817b23";
+const apiCountryURL = "https://flagcdn.com/w20/";
+
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
+
+const cityElement = document.querySelector("#city")
+const tempElement = document.querySelector("temperature span")
+const descElement = document.querySelector("#description")
+const weatherIconElement = document.querySelector("#weather-icon")
+const countryElement = document.querySelector("#country")
+const umidityElement = document.querySelector("#umidity span")
+const windElement = document.querySelector("#wind span")
+
+//funções
+
+const getWeatherData = async(city) => {
+    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`
+
+    const res = await fetch(apiWeatherURL);
+    const data = await res.json();
+
+    console.log(data);
+}
+
+const showWeatherData = (city) => {
+    getWeatherData(city);
+}
+
+//Eventos
+
+searchBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const city = cityInput.value;  
+
+    showWeatherData(city);
+})
