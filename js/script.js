485fd8085017e759862ea2b2dfa091bac70c4f9d
lucas-r-f <lucasrodriguesdefreitas2002@hotmail.com>
2024-08-15 15:13:00 -0300

    finish js

--- js/script.js
+++ js/script.js
@@ -1,43 +1,100 @@
-//Variáveis e seleção de elementos
 const apiKey = "78a58b51a528ac4982f8996d3f817b23";
-const apiCountryURL = "https://flagcdn.com/w20/";
+const apiCountryURL = "https://countryflagsapi.com/png/";
 
 const cityInput = document.querySelector("#city-input");
 const searchBtn = document.querySelector("#search");
 
-const cityElement = document.querySelector("#city")
-const tempElement = document.querySelector("temperature span")
-const descElement = document.querySelector("#description")
-const weatherIconElement = document.querySelector("#weather-icon")
-const countryElement = document.querySelector("#country")
-const umidityElement = document.querySelector("#umidity span")
-const windElement = document.querySelector("#wind span")
+const cityElement = document.querySelector("#city");
+const tempElement = document.querySelector("#temperature span");
+const descElement = document.querySelector("#description");
+const weatherIconElement = document.querySelector("#weather-icon");
+const countryElement = document.querySelector("#country");
+const umidityElement = document.querySelector("#umidity span");
+const windElement = document.querySelector("#wind span");
 
-//funções
+const weatherContainer = document.querySelector("#weather-data");
 
-const getWeatherData = async(city) => {
-    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`
+const errorMessageContainer = document.querySelector("#error-message");
+const loader = document.querySelector("#loader");
 
-    const res = await fetch(apiWeatherURL);
-    const data = await res.json();
+const suggestionContainer = document.querySelector("#suggestions");
+const suggestionButtons = document.querySelectorAll("#suggestions button");
 
-    return data;
+// Loader
+const toggleLoader = () => {
+  loader.classList.toggle("hide");
+};
+
+const getWeatherData = async (city) => {
+  toggleLoader();
+
+  const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+
+  const res = await fetch(apiWeatherURL);
+  const data = await res.json();
+
+  toggleLoader();
+
+  return data;
+};
+
+// Tratamento de erro
+const showErrorMessage = () => {
+  errorMessageContainer.classList.remove("hide");
+};
+
+const hideInformation = () => {
+  errorMessageContainer.classList.add("hide");
+  weatherContainer.classList.add("hide");
+
+  suggestionContainer.classList.add("hide");
 };
 
 const showWeatherData = async (city) => {
-    const data = getWeatherData(city);
+  hideInformation();
 
-    console.log(getWeatherData);
-    cityElement.innerText = data.name;
-    descElement.innerText = data.weather[0].description;
+  const data = await getWeatherData(city);
+
+  if (data.cod === "404") {
+    showErrorMessage();
+    return;
+  }
+
+  cityElement.innerText = data.name;
+  tempElement.innerText = parseInt(data.main.temp);
+  descElement.innerText = data.weather[0].description;
+  weatherIconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+  );
+  countryElement.setAttribute("src", apiCountryURL + data.sys.country);
+  umidityElement.innerText = `${data.main.humidity}%`;
+  windElement.innerText = `${data.wind.speed}km/h`;
+
+  weatherContainer.classList.remove("hide");
 };
 
-//Eventos
+searchBtn.addEventListener("click", async (e) => {
+  e.preventDefault();
 
-searchBtn.addEventListener("click", (e) => {
-    e.preventDefault();
+  const city = cityInput.value;
 
-    const city = cityInput.value;  
+  showWeatherData(city);
+});
+
+cityInput.addEventListener("keyup", (e) => {
+  if (e.code === "Enter") {
+    const city = e.target.value;
 
     showWeatherData(city);
+  }
+});
+
+// Sugestões
+suggestionButtons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const city = btn.getAttribute("id");
+
+    showWeatherData(city);
+  });
 });
